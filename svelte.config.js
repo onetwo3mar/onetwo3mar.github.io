b0ad6754166acde3913e8cf45f584bdcdb9de7e0
omar-nema <omarwnema@gmail.com>
2025-01-25 15:01:19 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,24 +3,18 @@ import adapter from '@sveltejs/adapter-static';
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
-
 const config = {
   kit: {
     adapter: adapter({
       pages: 'docs',
       assets: 'docs',
-      fallback: null,
-      precompress: false,
+      fallback: null
     }),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-
-    // paths: {
-    //   base: dev ? '' : '',
-    // },
-    appDir: 'internal',
-  },
+    paths: {
+      base: dev ? '' : '/onetwo3mar.github.io'
+    },
+    appDir: 'internal'
+  }
 };
 
 export default config;
